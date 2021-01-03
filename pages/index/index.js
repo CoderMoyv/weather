48a36a9d69d180b42e625960c7e678604a266440
moyv <1354760865@qq.com>
2021-01-03 13:32:25 +0800

    完成 进度 100%

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,4 @@
-const APIKEY = "8f4741418fb7419c9d472c8c8c87fc75"
+const APIKEY = "";// 填入你申请的KEY
 Page({
 
   /**
@@ -13,6 +13,56 @@ Page({
    */
   onLoad: function (options) {
     this.getLocation()
+  },
+  //选择定位
+  selectLocation() {
+    var that = this
+    wx.chooseLocation({
+      success(res) {
+        //console.log(res)
+        that.setData({
+          location: res.longitude + "," + res.latitude
+        })
+        that.getWeather()
+        that.getCityByLoaction()
+      }
+      , fail() {
+        wx.getLocation({
+          type: 'gcj02',
+          fail() {
+            wx.showModal({
+              title: '获取地图位置失败',
+              content: '为了给您提供准确的天气预报服务,请在设置中授权【位置信息】',
+              success(mRes) {
+                if (mRes.confirm) {
+                  wx.openSetting({
+                    success: function (data) {
+                      if (data.authSetting["scope.userLocation"] === true) {
+                        that.selectLocation()
+                      } else {
+                        wx.showToast({
+                          title: '授权失败',
+                          icon: 'none',
+                          duration: 1000
+                        })
+                      }
+                    }, fail(err) {
+                      console.log(err)
+                      wx.showToast({
+                        title: '唤起设置页失败，请手动打开',
+                        icon: 'none',
+                        duration: 1000
+                      })
+                    }
+                  })
+                }
+              }
+            })
+          }
+        })
+
+      }
+    })
   },
   /**
    * 获取定位
@@ -41,8 +91,7 @@ Page({
                       icon: 'success',
                       duration: 1000
                     })
-                    that.getWeather()
-                    that.getCityByLoaction()
+                    that.getLocation()
                   } else {
                     wx.showToast({
                       title: '授权失败',
@@ -99,7 +148,7 @@ Page({
         } else {
           wx.showToast({
             title: '获取城市信息失败',
-            icon: 'error'
+            icon: 'none'
           })
         }
 
@@ -111,16 +160,68 @@ Page({
    */
   getWeather() {
     var that = this
+    wx.showLoading({
+      title: '加载中',
+    })
     wx.request({
       url: 'https://devapi.qweather.com/v7/weather/now?key=' + APIKEY + "&location=" + that.data.location,
       success(result) {
         var res = result.data
-        console.log(res)
+        //console.log(res)
         that.setData({
           now: res.now
         })
       }
     })
+    wx.request({
+      url: 'https://devapi.qweather.com/v7/weather/24h?key=' + APIKEY + "&location=" + that.data.location,
+      success(result) {
+        var res = result.data
+        //console.log(res)
+        res.hourly.forEach(function (item) {
+          item.time = that.formatTime(new Date(item.fxTime)).hourly
+        })
+        that.setData({
+          hourly: res.hourly
+        })
+      }
+    })
+    wx.request({
+      url: 'https://devapi.qweather.com/v7/weather/7d?key=' + APIKEY + "&location=" + that.data.location,
+      success(result) {
+        var res = result.data
+        //console.log(res)
+        res.daily.forEach(function (item) {
+          item.date = that.formatTime(new Date(item.fxDate)).daily
+          item.dateToString = that.formatTime(new Date(item.fxDate)).dailyToString
+        })
+        that.setData({
+          daily: res.daily
+        })
+        wx.hideLoading()
+      }
+    })
+  },
+  // 格式时间
+  formatTime(date) {
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    const second = date.getSeconds()
+    const weekArray = ["周日", "周一", "周二", "周三", "周四", "周五", "周六"]
+    const isToday = date.setHours(0, 0, 0, 0) == new Date().setHours(0, 0, 0, 0)
+    return {
+      hourly: [hour, minute].map(this.formatNumber).join(":"),
+      daily: [month, day].map(this.formatNumber).join("-"),
+      dailyToString: isToday ? "今天" : weekArray[date.getDay()]
+    }
+  },
+  // 补零
+  formatNumber(n) {
+    n = n.toString()
+    return n[1] ? n : '0' + n
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
